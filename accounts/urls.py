569119dee0d64e03cf054768e4f569cbaa92eef5
ElitsaY <eyotkova@gmail.com>
2024-12-22 16:41:14 +0200

    enable student login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import RegisterStudentView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, 
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('register/', RegisterStudentView.as_view(), name='register_student'),
+     # JWT Login
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT Refresh
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
